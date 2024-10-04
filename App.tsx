217001fc90552a702a51cb2fa70f52a86e706d00
Đoàn Phương Nam <120766900+DPhngNam@users.noreply.github.com>
2024-10-05 00:40:56 +0700

    init

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Button } from '@rneui/themed';
+import React from 'react';
+import { View } from 'react-native';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-white">
+      <Button title="Solid" />
+      <Button title="Outline" type="outline" />
+      <Button title="Clear" type="clear" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
