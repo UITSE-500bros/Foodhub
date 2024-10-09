3b3c5d47a9b79095235e974df741462bc756b4f0
ThInhHuYnh04 <22521406@gm.uit.edu.vn>
2024-10-09 10:20:49 +0700

    feat: custom Button

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import { Button } from '@rneui/themed';
+
 import React from 'react';
 import { View } from 'react-native';
+import Button from './src/components/Button';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Button title="Solid" />
-      <Button title="Outline" type="outline" />
-      <Button title="Clear" type="clear" />
+      <Button title='Press me' onPress={() => console.log('Button pressed')} width={1000} height={50} />
     </View>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,28 @@
+import { color } from "@rneui/base";
+import React from "react";
+import { View, Text, Touchable, TouchableOpacity } from "react-native";
+import { StyleSheet } from "react-native";
+
+interface ButtonProps {
+  title: string;
+  onPress: () => void;
+  width?: number;
+  height?: number;
+}
+
+const Button: React.FC<ButtonProps> = ({ title, onPress, width, height }) => {
+  return (
+    <TouchableOpacity style={[{ width, height},styles.button]} onPress={onPress}>
+      <Text className=" text-white font-normal">{title}</Text>
+    </TouchableOpacity>
+  );
+};
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#53B175',
+        borderRadius: 19,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    });
+export default Button;
