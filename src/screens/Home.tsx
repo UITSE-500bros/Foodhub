052f70e0b6481224057ff8648f20d5c3d3146a97
ThInhHuYnh04 <22521406@gm.uit.edu.vn>
2024-10-15 23:43:55 +0700

    feat: hardcode product list

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,7 @@
 import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
-import { Searchbar } from "react-native-paper";
-
+import { Button, Searchbar } from "react-native-paper";
+import ProductCardSquare from "../components/ProductCardSquare";
 
 const Home = () => {
   return (
@@ -11,24 +11,45 @@ const Home = () => {
         className=" w-[30px] h-[30px] "
       />
       {/* search bar */}
-      <View className="w-[364] h-[52] shrink-0  bg-white">
-        <Searchbar 
-            placeholder="Search..."
-            value=""
-            onChangeText={()=>{}}
-            style={{borderRadius: 10,backgroundColor: '#F9F9F9'}}
-            />
-     
+      <View className="w-full h-[52] px-4   bg-white ">
+        <Searchbar
+          placeholder="Search Store"
+          value=""
+          onChangeText={() => {}}
+          style={{ borderRadius: 15, backgroundColor: "#F9F9F9",width:"100%" }}
+        />
       </View>
- 
-   
-     
+
+        {/* product cards */}
+      <View className="w-full px-4">
+        <View className="flex flex-row items-center justify-between w-full">
+          <Text className=" text-[#181725] text-2xl font-semibold ">
+            Exclusive Offer
+          </Text>
+          <Button mode="text" labelStyle={styles.buttonText}>
+            See All
+          </Button>
+        </View>
+        <View className="flex flex-row justify-between">
+          <ProductCardSquare />
+            <ProductCardSquare />
+          
+          </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-
-    });
+  buttonText: {
+    color: "#53B175",
+    fontSize:16,
+    fontWeight: 600,
+    fontStyle: "normal",
+    lineHeight: 24,
+    
+    
+  },
+});
 
 export default Home;
