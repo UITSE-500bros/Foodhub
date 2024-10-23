95da5d9a9ce301832dd4e519130ea00dc15a1638
Đoàn Phương Nam <120766900+DPhngNam@users.noreply.github.com>
2024-10-24 00:09:41 +0700

    fix: category component

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from "react";
 import * as Font from "expo-font";
 import {SafeAreaProvider} from "react-native-safe-area-context";
 import {ProductDetail,Home,Explore,CategoryDetail,Favorite,Cart,Search } from "./src/screens"
+import { Category } from "./src/components";
 
 const loadFonts = async () => {
   await Font.loadAsync({
