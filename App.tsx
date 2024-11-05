88a7cf52bcb75dba776f8b0bfde343802a7ddd44
ThInhHuYnh04 <22521406@gm.uit.edu.vn>
2024-11-05 23:28:33 +0700

    feat: bottom bar icon

--- App.tsx
+++ App.tsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 import * as Font from "expo-font";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import LocationRegister from "./src/screens/LocationRegister/LocationRegister";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { NavigationContainer } from "@react-navigation/native";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import Home from "./src/screens/Home";
 import Cart from "./src/screens/Cart";
 import Favorite from "./src/screens/Favorite";
 import Explore from "./src/screens/Explore";
-
 
 const loadFonts = async () => {
   await Font.loadAsync({
@@ -32,14 +32,47 @@ export default function App() {
     <NavigationContainer>
       <SafeAreaProvider>
         <Tab.Navigator>
-         
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="Explore" component={Explore} />
-          <Tab.Screen name="Cart" component={Cart} />
-          <Tab.Screen name="Favorite" component={Favorite} />
-          <Tab.Screen name="Profile" component={LocationRegister} />
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              tabBarLabel: "Home",
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="home" color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Explore"
+            component={Explore}
+            options={{
+              tabBarLabel: "Explore",
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons
+                  name="compass"
+                  color={color}
+                  size={26}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen name="Cart" component={Cart} options={{
+            tabBarLabel: "Cart",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="cart" color={color} size={26} />
+            ),
+          }} />
+          <Tab.Screen name="Favorite" component={Favorite} options={{
+            tabBarLabel: "Favorite",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="heart" color={color} size={26} />),
+          }} />
+          <Tab.Screen name="Profile" component={LocationRegister} options={{
+            tabBarLabel: "Profile",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account" color={color} size={26} />),
+          }} />
         </Tab.Navigator>
-
       </SafeAreaProvider>
     </NavigationContainer>
   );
