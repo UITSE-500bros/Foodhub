5adc2001de8aedc3dba93207abe03408ed84a548
DPhngNam <22520908@gm.uit.edu.vn>
2024-12-22 14:57:58 +0700

    feat: update package

--- App.tsx
+++ App.tsx
@@ -1,85 +1,14 @@
-import { API_URL, GOOGLE_ANDROID_CLIENT_ID } from "@env";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { NavigationContainer } from "@react-navigation/native";
-import * as Google from "expo-auth-session/providers/google";
 import * as WebBrowser from "expo-web-browser";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import RootStack from "./src/navigation/RootStack";
-import Login from "./src/screens/Login";
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
-  const [userInfo, setUserInfo] = useState(null);
-  const [token, setToken] = useState("");
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    androidClientId: GOOGLE_ANDROID_CLIENT_ID,
-    scopes: ["profile", "email"],
-  });
-
-  useEffect(() => {
-    handleEffect();
-  }, [response, token]);
-
-  async function handleEffect() {
-    const user = await getLocalUser();
-    if (!user) {
-      if (response?.type === "success") {
-        if (response.authentication) {
-          getUserInfo(response.authentication.accessToken);
-        }
-      }
-    } else {
-      setUserInfo(user);
-      console.log("loaded locally");
-    }
-  }
-  const getLocalUser = async () => {
-    const data = await AsyncStorage.getItem("@user");
-    if (!data) return null;
-    return JSON.parse(data);
-  };
-
-  const getUserInfo = async (token: string) => {
-    if (!token) return;
-    try {
-      const response = await fetch(
-        "https://www.googleapis.com/userinfo/v2/me",
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      );
-
-      const user = await response.json();
-      await sendUserInfoToServer(user);
-      // await AsyncStorage.setItem("@user", JSON.stringify(user));
-      setUserInfo(user);
-    } catch (error) {}
-  };
-
-  const sendUserInfoToServer = async (user: any) => {
-    try {
-      let url = API_URL + "/user/auth/google";
-      console.log(url);
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
-      });
-
-      if (!response.ok) {
-        throw new Error("Failed to send user info to server");
-      }
-
-      console.log("User info sent to server successfully");
-    } catch (error) {
-      console.error("Error sending user info to server:", error);
-    }
-  };
+  
 
   return (
     // !userInfo ?
