e7e2419495cb3e7f991c67c8ffc9b3c0d5df1d22
DPhngNam <22520908@gm.uit.edu.vn>
2025-01-12 00:27:47 +0700

    feat: cart bottom

--- src/screens/Cart/Cart.tsx
+++ src/screens/Cart/Cart.tsx
@@ -1,75 +1,147 @@
-import React, { useEffect, useState } from "react";
-import {
-  Dimensions,
-  FlatList,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { Dimensions, FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ProductCard } from "../../components";
-import { usersService } from "../../service";
+import { Icon } from "@rneui/themed";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 import { useNavigation } from "@react-navigation/native";
 import { VNpayScreenNavigationProp } from "../../../type";
+import { ProductCard } from "../../components";
 import useCartStore from "./store/CartStore";
+import { usersService } from "../../service";
 
-const Cart = () => {
-  const [total, setTotal] = useState(0);
-  const width = Dimensions.get("window").width;
-  const height = Dimensions.get("window").height;
+const InfoSection: React.FC<{ title: string; children: React.ReactNode; onPress?: () => void; stroke?: boolean }> = ({ title, children, onPress, stroke = true }) => (
+  <TouchableOpacity onPress={onPress} style={styles.infoSection}>
+    <Text style={styles.sectionTitle}>{title}</Text>
+    {children}
+  </TouchableOpacity>
+);
 
+const PaymentMethod: React.FC<{handleGoBack: () => void}> = ({ handleGoBack }) => (
+  <BottomSheetView style={styles.methodItem}>
+    <Text style={styles.methodText}>VNPAY</Text>
+    <Icon name="right" type="antdesign" size={20} onPress={handleGoBack}/>
+  </BottomSheetView>
+);
+
+const PromotionCode: React.FC<{handleGoBack: () => void}> = ({ handleGoBack }) => (
+  <BottomSheetView style={styles.methodItem}>
+
+    <Text style={styles.methodText}>PROMO codes</Text>
+    <Icon name="right" type="antdesign" size={20} onPress={handleGoBack}/>
+  </BottomSheetView>
+);
+
+const Main: React.FC<{ handleClosePress: () => void; openPayment: () => void }> = ({ handleClosePress, openPayment }) => (
+  <BottomSheetView style={styles.mainContainer}>
+    <View style={styles.header}>
+      <Text style={styles.headerTitle}>Thanh toán 🎉</Text>
+      <Icon name="close" type="fontisto" size={30} onPress={handleClosePress} />
+    </View>
+    <InfoSection title="Phương thức giao hàng" onPress={openPayment}>
+      <View style={styles.details}>
+        <Text style={styles.text}>Giao hàng tiêu chuẩn</Text>
+        <Text style={styles.subText}>Miễn phí</Text>
+      </View>
+    </InfoSection>
+    <InfoSection title="Phương thức thanh toán" onPress={openPayment}>
+      <Text style={styles.text}>Giao hàng tiêu chuẩn</Text>
+      <Icon name="right" type="antdesign" size={20} />
+    </InfoSection>
+    <InfoSection title="Mã giảm giá" onPress={openPayment}>
+      <Text style={styles.text}>Giao hàng tiêu chuẩn</Text>
+      <Icon name="right" type="antdesign" size={20} />
+    </InfoSection>
+    <InfoSection title="Tổng tiền" stroke={false}>
+      <Text style={styles.text}>Giao hàng tiêu chuẩn</Text>
+    </InfoSection>
+    <TouchableOpacity style={styles.paymentButton}>
+      <Text style={styles.paymentText}>Thanh toán</Text>
+    </TouchableOpacity>
+  </BottomSheetView>
+);
+
+const Cart: React.FC = () => {
+  const { cart, updateQuantity } = useCartStore(state => state);
   const nav = useNavigation<VNpayScreenNavigationProp>();
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const [total, setTotal] = useState(0);
+  const handleClosePress = useCallback(() => bottomSheetRef.current?.close(), []);
 
+  const handleGoBack = () => {
+    console.log("handleGoBack");
+    setBottomSheetContent(<Main handleClosePress={handleClosePress} openPayment={openPayment} />);
+    bottomSheetRef.current?.expand();
+  };
+  const openPayment = () => {
+    setBottomSheetContent(<PaymentMethod handleGoBack={handleGoBack}/>);
+    bottomSheetRef.current?.expand();
+  };
+  const [bottomSheetContent, setBottomSheetContent] = useState<React.ReactNode>();
+
+  useEffect(() => {
+    const totalAmount = cart.reduce((acc, item) => acc + item.product_price * item.quantity, 0);
+    setTotal(totalAmount);
+  }, [cart]);
+
+  const snapPoints = useMemo(() => ["50%", "75%"], []);
+  
+  
   const handlePayment = async () => {
     try {
       const url = await usersService.checkout(50000);
-
-      if (url) {
-        nav.navigate("VNpay", { uri: url });
-      }
+      if (url) nav.navigate("VNpay", { uri: url });
     } catch (error) {
       console.error("Error presenting payment sheet:", error);
     }
   };
-  const cartItems = useCartStore((state) => state.cart);
-  const updateQUantity = useCartStore((state) => state.updateQuantity);
-  const handleQuantityChange = (id: string, quantity: number) => {
-    updateQUantity(id, quantity);
-  }
+
   return (
-    <SafeAreaView className="flex-1 px-4 ">
-      <View className="flex justify-center items-center mt-10">
-        <Text className="text-center text-[#181725] text-xl font-black">
-         Giỏ hàng
-        </Text>
-      </View>
-      <Text className="text-center text-[#7C7C7C] text-xl font-[900]">
-        Tổng tiền 35000
-      </Text>
-      <View>
-        <Text className="text-lg font-semibold">Địa chỉ giao hàng: </Text>
-        <Text>Phường Linh Trung, Quận Thủ Đức, TP.HCM</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerSection}>
+        <Text style={styles.cartTitle}>Giỏ hàng</Text>
+        <Text style={styles.totalText}>Tổng tiền {total}</Text>
+        <Text style={styles.addressLabel}>Địa chỉ giao hàng:</Text>
+        <Text style={styles.address}>Phường Linh Trung, Quận Thủ Đức, TP.HCM</Text>
       </View>
       <FlatList
-        data={cartItems}
-        className="mr-4"
-        style={{ width: width, height: height - 200 }}
+        data={cart}
         keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <View key={item.id} className="m-1">
-            <View className="h-[1px] bg-[#E5E5E5]" />
-            <ProductCard adjust   product={item} />
-          </View>
-        )}
+        renderItem={({ item }) => <ProductCard key={item.id} adjust product={item} />}
+        style={styles.cartList}
       />
-      <TouchableOpacity
-        className={`absolute h-[67px] bottom-0 flex items-center justify-center left-6 right-6 bg-[#53B175] rounded-2xl`}
-        onPress={handlePayment}
-      >
-        <Text className="text-white">Check out</Text>
+      <TouchableOpacity style={styles.orderButton} onPress={handleGoBack} className="bg-black">
+        <Text style={styles.orderButtonText}>Đặt hàng</Text>
       </TouchableOpacity>
+      <BottomSheet ref={bottomSheetRef} snapPoints={snapPoints} style={styles.bottomSheet}>
+        {bottomSheetContent}
+      </BottomSheet>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { flex: 1, paddingHorizontal: 16 },
+  headerSection: { alignItems: "center", marginTop: 20 },
+  cartTitle: { fontSize: 24, fontWeight: "bold", color: "#181725" },
+  totalText: { fontSize: 18, fontWeight: "900", color: "#7C7C7C" },
+  addressLabel: { fontSize: 18, fontWeight: "600" },
+  address: { fontSize: 16, color: "#333" },
+  cartList: { width: "100%", height: Dimensions.get("window").height - 250 },
+  orderButton: { position: "absolute", bottom: 20, left: 16, right: 16, height: 67, backgroundColor: "#53B175", borderRadius: 20, justifyContent: "center", alignItems: "center" },
+  orderButtonText: { color: "white", fontSize: 16 },
+  bottomSheet: { backgroundColor: "white" },
+  methodItem: { flexDirection: "row", justifyContent: "space-between", alignItems: "center", height: 62 },
+  methodText: { color: "black" },
+  mainContainer: { padding: 20, flexDirection: "column", height: "100%" },
+  header: { flexDirection: "row", justifyContent: "space-between", alignItems: "center", marginBottom: 10 },
+  headerTitle: { fontSize: 20, fontWeight: "600" },
+  infoSection: { flexDirection: "row", justifyContent: "space-between", alignItems: "center", height: 62 },
+  sectionTitle: { fontSize: 16, color: "black" },
+  details: { flexDirection: "column", alignItems: "flex-end" },
+  text: { fontSize: 16, color: "black" },
+  subText: { fontSize: 12, color: "gray" },
+  paymentButton: { height: 67, backgroundColor: "#53B175", borderRadius: 20, justifyContent: "center", alignItems: "center" },
+  paymentText: { color: "white", fontSize: 16 },
+});
 
 export default Cart;
--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -9,55 +9,55 @@ import * as WebBrowser from "expo-web-browser";
 import * as Linking from "expo-linking";
 import * as SecureStore from 'expo-secure-store';
 import { API_URL } from "@env";
+import { storeTokens } from "../../service/tokenEncrypted";
 
-WebBrowser.maybeCompleteAuthSession(); // required for web only
-
-const redirectTo = makeRedirectUri();
-const performOAuth = async (logintype: string) => {
-  try {
-    const response = await fetch(`${API_URL}/user/${logintype}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        accept: "*/*",
-      },
-    });
-    if (response.ok) {
-      const data = await response.json();
-      if (data.url) {
-        const res = await WebBrowser.openAuthSessionAsync(
-          data.url,
-          redirectTo
-        );
-        if (res.type === "success") {
-          const { url } = res;
-
-          // Save the accessToken to the secure store
-          const { params, errorCode } = QueryParams.getQueryParams(url);
-
-          if (errorCode) throw new Error(errorCode);
-          const { access_token, refresh_token } = params;
-          console.log(access_token, refresh_token);
-          if (access_token && refresh_token) {
-            await SecureStore.setItemAsync("access_token", access_token);
-            await SecureStore.setItemAsync("refresh_token", refresh_token);
-          } else {
-            console.error("No access token found in the response");
-          }
-        }
-      } else {
-        console.error("No URL found in the response");
-      }
-    } else {
-      console.error("Failed to fetch OAuth URL:", response.statusText);
-    }
-  } catch (error) {
-    console.error("Error during OAuth request:", error);
-  }
-};
-
+WebBrowser.maybeCompleteAuthSession();
 const Login = () => {
   const nav = useNavigation<LoginScreenNavigationProp>()
+  const redirectTo = makeRedirectUri();
+  const performOAuth = async (logintype: string) => {
+    try {
+      const response = await fetch(`${API_URL}/user/${logintype}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          accept: "*/*",
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        if (data.url) {
+          const res = await WebBrowser.openAuthSessionAsync(
+            data.url,
+            redirectTo
+          );
+          if (res.type === "success") {
+            const { url } = res;
+
+            // Save the accessToken to the secure store
+            const { params, errorCode } = QueryParams.getQueryParams(url);
+
+            if (errorCode) throw new Error(errorCode);
+            const { access_token, refresh_token } = params;
+
+            if (access_token && refresh_token) {
+              await storeTokens(access_token, refresh_token);
+              nav.navigate("PhoneNumber");
+            } else {
+              console.error("No access token found in the response");
+            }
+          }
+        } else {
+          console.error("No URL found in the response");
+        }
+      } else {
+        console.error("Failed to fetch OAuth URL:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error during OAuth request:", error);
+    }
+  };
+
   const url = Linking.createURL("login");
 
   return (
@@ -121,8 +121,7 @@ const Login = () => {
             marginVertical: 10,
           }}
           onPress={() => {
-            performOAuth('google');
-            // nav.navigate("PhoneNumber")
+            performOAuth('google')
           }}
         />
 
@@ -155,8 +154,7 @@ const Login = () => {
             marginVertical: 10,
           }}
           onPress={() => {
-            console.log("Facebook");
-            performOAuth('facebook');
+            performOAuth('facebook')
           }}
         />
       </View>
