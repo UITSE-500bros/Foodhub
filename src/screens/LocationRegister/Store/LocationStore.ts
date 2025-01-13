cded8c127fb73d37a5b3cb2c9c54fc36c3337b30
ThinhHuynhUIT <22521406@gm.uit.edu.vn>
2025-01-13 21:47:18 +0700

    feat:update location store

--- src/screens/LocationRegister/Store/LocationStore.ts
+++ src/screens/LocationRegister/Store/LocationStore.ts
@@ -1,15 +1,31 @@
 import { create } from "zustand";
+import axiosInstance from "../../../service/axiosInstance";
 
 
+
+type Location ={
+    
+}
 interface LocationStore {
     locations: string[];
     addLocation: (location: string) => void;
     removeLocation: (location: string) => void;
     locationCount: number;
+    fetchLocation: ()=>Promise<void>;
 }
 
 const useLocationStore = create<LocationStore>((set) => ({
     locations: [],
+    fetchLocation: async () => {
+        try{
+            const response = await axiosInstance.get('/user/address');
+            set({locations: response.data})
+        }
+        catch(e){
+            console.error('Error fetching locations', e)
+        }
+            
+    },
     addLocation: (location) => set((state) => ({ locations: [...state.locations, location] })),
     removeLocation: (location) => set((state) => ({ locations: state.locations.filter(loc => loc !== location) })),
     get locationCount() {
